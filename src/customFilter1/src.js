f49a9a5f1eb2c885a4195a348632acf636a66df1
jinouwuque <ee2win@gmail.com>
2016-08-29 00:13:06 -0400

    add some filter example

--- src/customFilter1/src.js
+++ src/customFilter1/src.js
@@ -1,7 +1,24 @@
 angular.module('app',[])
 .controller('ctr', ['$scope', function($scope){
         angular.merge($scope, {
-           test: 'here'
+            list: [
+                {name: 'Tom', age: 10},
+                {name: 'Jerry', age: 21},
+                {name: 'Steve', age: 32},
+                {name: 'Jefferson', age: 44}
+            ]
         });
 
+    }])
+.filter('customeFilter', [function(){
+        return function(list, num){
+            var result = list;
+            if(typeof num == 'number'){
+                result = list.filter(function(v,k){
+                    return v.age % num === 0;
+                });
+            }
+
+            return result;
+        };
     }]);
