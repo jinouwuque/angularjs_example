8e146c72aaf62b636ac2a7dd28d0950b29d8e2e8
jinouwuque <ee2win@gmail.com>
2016-08-29 00:37:06 -0400

    add some phase benchmark on the directive creating

--- src/directiveController/src.js
+++ src/directiveController/src.js
@@ -4,4 +4,31 @@ angular.module('app',[])
            test: 'here'
         });
 
-    }]);
+    }])
+    .directive("simple", function(){
+        return {
+            restrict: "EA",
+            //transclude:true,
+            template:"<div>{{label}}<div ng-transclude></div></div>",
+            compile: function(element, attributes){
+                console.log("angular compile the directive");
+                return {
+                    pre: function(scope, element, attributes, controller, transcludeFn){
+                        debugger;
+                    },
+                    post: function(scope, element, attributes, controller, transcludeFn){
+                        debugger;
+                    }
+                };
+                // compilation phase is Angular returns the template function
+            },
+            link: function(scope, element, attrs, controller, transcludeFn){
+                debugger;
+                // attacth the data to the directive
+            },
+            controller: function($scope){
+                debugger;
+
+            }
+        };
+    });
